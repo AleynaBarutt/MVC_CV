598113f85f555b2fa79c1450bb099edb4ec7b29c
aleyna <aleynaebrt@gmail.com>
2022-11-08 15:20:19 +0300

    PartialView kullanımı ve View sayfalarının kodlanması

--- MvcCv/Controllers/DefaultController.cs
+++ MvcCv/Controllers/DefaultController.cs
@@ -16,5 +16,48 @@ namespace MvcCv.Controllers
             var degerler = db.tblHakkimda.ToList();
             return View(degerler);
         }
+
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.tblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimler = db.tblEgitim.ToList();
+            return PartialView(egitimler);
+        }
+
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.tblYeteneklerim.ToList();
+            return PartialView(yetenekler);
+        }
+
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.tblHobilerim.ToList();
+            return PartialView(hobiler);
+        }
+        public PartialViewResult Sertifikalar()
+        {
+            var sertifikalar = db.tblSertifikalarim.ToList();
+            return PartialView(sertifikalar);
+        }
+
+        [HttpGet]
+        public PartialViewResult Iletisim()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Iletisim(tblİletisim t)
+        {
+            t.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.tblİletisim.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
     }
 }
