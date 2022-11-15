3413e09f3972798860a715f29799b92fb637fe15
aleyna <aleynaebrt@gmail.com>
2022-11-15 15:58:32 +0300

    Sertifika Hobi İletişim Sayfaları Tamamlandı

---
+++ MvcCv/Controllers/HobiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+namespace MvcCv.Controllers
+{
+    public class HobiController : Controller
+    {
+        // GET: Hobi
+        GenericRepository<tblHobilerim> repo = new GenericRepository<tblHobilerim>();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var hobi = repo.List();
+            return View(hobi);
+        }
+        [HttpPost]
+        public ActionResult Index(tblHobilerim p) { 
+            var t = repo.Find(x => x.ID == 1);
+            t.Aciklama1 = p.Aciklama1;
+            t.Aciklama2 = p.Aciklama2;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
+    }
+}
